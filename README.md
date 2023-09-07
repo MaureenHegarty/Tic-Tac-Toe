752bfb27454ce91158685b3f6645c0b605f49523
Maureen Hegarty <maureen@communus.com.au>
2023-09-07 21:36:44 +1000

    updates

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# Tic-Tac-Toe
+# Barbie Tic-Tac-Toe
 [game-link](https://mauzzah.github.io/Tic-Tac-Toe/)
+
+# Project Name
+Barbie Tic Tac Toe
+
+## [Click here](#) to see my live project!
+
+## About
+- This is a traditional game of Tic-Tac-Toe, or noughts and crosses which displays the symbols of X’s and O’s, as two players take turns clicking on the boxes of a three-by-three grid. The player who gets three of their symbols in a horizontal, vertical, or diagonal row is the winner.
+$ git clone my-awesome-app
+$ cd my-awesome-app
+$ node app.js
+```
+! [in-app screenshot]( https://github.com/MauzzaH/Tic-Tac-Toe/blob/main/Barbie-Tic-Tac-Toe.png)
+
+## Planning & Problem Solving
+- my starting point was breaking down the global variables, event listeners, util functions, and event handlers.  
+- the global variables to me were the cells in the table, the buttons, and the possible winning combinations by player X or O
+- I numbered the boxes 1-9
+- then I started with player X and “if/else if” statements and operators
+- however this soon looked to be a very inefficient way of writing the code for a simple game with a set number of winning combos (8) and turn-taking between 2 players. Also the way I was writing the code, the variables were all specific to each function and did not seem to be global.
+- I investigated alternatives, both by googling and speaking with my neighbour. 
+- I went back to the 4 sections and started with the global variables, making the boxes(cells), the box values, and the winning combos all arrays and all const’s.
+- then I added the event listener of clicking on the boxes
+- for the checkWinner function I used the logical AND operator after looping through the winning combos array and searched online how to record the incrementing wins of both player X and player O
+- I googled the reset button and used the round value and searched online how to alternate players
+
+
+## Cool tech
+- downloaded a free mp3 to display when the winning conditions were met
+- made gifs for the x’s o’s and rotating barbie
+
+
+## Bugs to fix
+- reset game button can be clicked when no game has been played, so it looks like Round 4, Round 5 etc. when no round/game has been played
+- the round count only starts counting AFTER the first round so it is always incrementing one round behind
+
+## Lessons learnt
+- Keep better notes along the way for a comprehensive wireframe and track what worked and what didn’t 
+
+## Future features
+- have a “Try Again” graphic which displays after the match is tied that is different from just clicking the reset button
+- include a timer
+- include a volume button as the audio file gets to be annoying
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- apps.js
+++ apps.js
@@ -1,27 +1,27 @@
 //global variables  ==================================
-
+//boxes, boxValues and winningCombinations are all stored in arrays
 const boxes = Array.from(document.querySelectorAll(".cell"));
 const boxValues = [];
 for (let i = 0; i < boxes.length; i++) {
   boxValues[i] = "";
 }
 const winningCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+  [0, 1, 2],  /* across the top */
+  [3, 4, 5],  /* across the middle */
+  [6, 7, 8],  /* across the bottom */
+  [0, 3, 6],  /* left vertical */
+  [1, 4, 7],  /* middle vertical */
+  [2, 5, 8],  /* right vertical */
+  [0, 4, 8],  /* tLeft to bRight diagonal */
+  [2, 4, 6]   /* tRight to bLeft diagonal */
 ];
+//round is the variable which is keeping track of the current number of games - it increments by 1 each time the resetGame function is called
 let xWins = 0;
 let oWins = 0;
 let round = 0;
 
-
 //set event listeners =============================
-
+//the for loop iterates over the boxes array, for each box element the eventListener is added that will call the handleClick function when the box is clicked.
 for (let i = 0; i < boxes.length; i++) {
     boxes[i].addEventListener("click", handleClick);
 }
@@ -51,19 +51,19 @@ function displayO(button) {
         img.src = "assets/O Tic Tac Toe static.png"; // I have replaced the path to your static O image
     }, 1000);
 }
-
+// The disableAllBoxes function is called when there is a winner or a tie in the game
 function disableAllBoxes() {
    for (let i = 0; i < boxes.length; i++) {
      boxes[i].disabled = true;
    }
 }
-
+// values of a,b, and c are determined in the checkWinner()function which loops through the winningCombinations array - for each winning combination a,b and c are set to the indices of the boxes in that combination and if they match ie all "x's" or all "o's" there is a winner and this function is called to make the contents red.
 function colorBoxes(a,b,c) {
    boxes[a].style.color = "red";
    boxes[b].style.color = "red";
    boxes[c].style.color = "red";
 }
-
+// after checkWinner() function loops through winningCombinations and there is no winner the isTie(boxValues) function is called to check if game has ended in a tie. if "true" is returned the checkWinner() function updates the display and calls the disableAll Boxes()function.
 function isTie(boxValues) {
    for (let i = 0; i < boxValues.length; i++) {
      if (boxValues[i] === "") {
@@ -75,36 +75,72 @@ function isTie(boxValues) {
 
 function resetGame() {
     round++;
+    //resets all the boxes to an empty string with no "x's" or "o's" so a new game can be started. it increments the round variable to show the new value of round ie number of games played and iterates over the boxes to reset the property to an empty string. also sets "disabled" to false to enable user input to box
     document.getElementById("round").innerHTML = round;
     for (let i = 0; i < boxes.length; i++) {
-        boxes[i].innerHTML = "";
+        boxes[i].innerHTML = ""; 
         boxes[i].value = "";
         boxes[i].disabled = false;
         boxValues[i] = "";
     }
     document.getElementById("print").innerHTML = "";
 }
-
+ 
 
 function checkWinner() {
-    // Check for a winner here
-    if (currentPlayer === "x") {
-        xWins++;
-        document.getElementById("xWins").innerHTML = xWins;
-        resetGame();
-    } else if (currentPlayer === "0") {
-        oWins++;
-        document.getElementById("oWins").innerHTML = oWins;
-        resetGame();
+    // Loop through the winning combinations and compare the values of the buttons. for each winning combo the function checks if the values of the boxes are all "x's" or all "o's" - if they are the function increments the xWins or oWins variable, updates who has won (X or O), disables the boxes, and returns from the functon. if no winner, the function checks if there's a tie by calling the isTie function.
+    for (let i = 0; i < winningCombinations.length; i++) {
+       const a = winningCombinations[i][0];
+       const b = winningCombinations[i][1];
+       const c = winningCombinations[i][2];
+        // use logical AND operator that returns true if both operands are true, and here it checks if all 3 conditions are true. if so player X has won.
+       if (boxValues[a] === "x" && boxValues[b] === "x" && boxValues[c] === "x") {
+         xWins++;
+         document.getElementById("xWins").innerHTML = xWins;
+         document.getElementById("print").innerHTML = "Player X is the Winner!";
+         disableAllBoxes();
+         colorBoxes(a,b,c);
+
+        document.getElementById("winning-image").style.display = "block";
+         setTimeout(() => {
+        document.getElementById("winning-image").style.display = "none";
+        }, 5000);
+        document.getElementById("winning-audio").play();
+
+
+         return;
+
+       } else if (boxValues[a] === "0" && boxValues[b] === "0" && boxValues[c] === "0") {
+         oWins++;
+         document.getElementById("oWins").innerHTML = oWins;
+         document.getElementById("print").innerHTML = "Player O is the Winner!";
+         disableAllBoxes();
+         colorBoxes(a,b,c);
+
+         document.getElementById("winning-image").style.display = "block";
+         setTimeout(() => {
+        document.getElementById("winning-image").style.display = "none";
+        }, 5000);
+        document.getElementById("winning-audio").play();
+         return;
+       }
+    }
+ 
+    // If there is no winner, check if there is a tie
+    
+    if (isTie(boxValues)) {
+       document.getElementById("print").innerHTML = "Match Tie";
+       disableAllBoxes();
+       return;
     }
 }
 
-
 //event handlers =====================================
-
+// handleClick function is triggered when a player clicks on a box in the game. this function checks whose turn it is, displays an "X" or "O", updates the game state and checks if there is a winner or a tie. it also alternates the current player for the next turn.
 let currentPlayer = "x"; 
 
 function handleClick(event) {    
+    // the button variable is the box clicked by the player and is set to the target property of the event obect.
     const button = event.target;
     
     if (currentPlayer === "x") {
@@ -128,53 +164,5 @@ function handleClick(event) {
         currentPlayer = "0";
     } else if (currentPlayer === "0") {
         currentPlayer = "x";
-    }
- }
-
-
- function checkWinner() {
-    // Loop through the winning combinations and compare the values of the buttons
-    for (let i = 0; i < winningCombinations.length; i++) {
-       const a = winningCombinations[i][0];
-       const b = winningCombinations[i][1];
-       const c = winningCombinations[i][2];
- 
-       if (boxValues[a] === "x" && boxValues[b] === "x" && boxValues[c] === "x") {
-         xWins++;
-         document.getElementById("xWins").innerHTML = xWins;
-         document.getElementById("print").innerHTML = "Player X is the Winner!";
-         disableAllBoxes();
-         colorBoxes(a,b,c);
-
-        document.getElementById("winning-image").style.display = "block";
-         setTimeout(() => {
-        document.getElementById("winning-image").style.display = "none";
-        }, 5000);
-        document.getElementById("winning-audio").play();
-
-
-         return;
-       } else if (boxValues[a] === "0" && boxValues[b] === "0" && boxValues[c] === "0") {
-         oWins++;
-         document.getElementById("oWins").innerHTML = oWins;
-         document.getElementById("print").innerHTML = "Player O is the Winner!";
-         disableAllBoxes();
-         colorBoxes(a,b,c);
-
-         document.getElementById("winning-image").style.display = "block";
-         setTimeout(() => {
-        document.getElementById("winning-image").style.display = "none";
-        }, 5000);
-        document.getElementById("winning-audio").play();
-         return;
-       }
-    }
- 
-    // If there is no winner, check if there is a tie
-    
-    if (isTie(boxValues)) {
-       document.getElementById("print").innerHTML = "Match Tie";
-       disableAllBoxes();
-       return;
     }
 }
